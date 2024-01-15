f2125d10e8e6b69db2b780899982cd3ec4046d34
Djem Mustafa <djemko123@gmail.com>
2024-01-15 20:43:42 +0200

    Test evaluation done as well

--- test_evaluation.py
+++ test_evaluation.py
@@ -11,20 +11,29 @@ def evaluate_test():
         'answers')  # TODO {"answers": [{"question_id": "..", "option_id": ".."}, ...]}
 
     with Session(engine) as session:
-        incorrect_answers = []
+        evaluation_response = []
 
         for answer in user_answers:
             question = session.query(Question).get(answer['question_id'])
             selected_option = session.query(Option).get(answer['option_id'])
+            correct_option = session.query(Option).filter_by(question_id=question.id, is_correct=True).first()
 
-            if not selected_option.is_correct:
-                incorrect_answers.append({
-                    "question_id": question.id,
-                    "question_title": question.title,
-                    "answer_description": question.answer_description
-                    # TODO: Add selected_option and the correct option itself. + Question Difficulty?
-                })
+            evaluation_response.append({
+                "question_id": question.id,
+                "question_title": question.title,
+                "answer_description": question.answer_description,
+                "selected_option": serialize_option(selected_option),
+                "correct_option": serialize_option(correct_option)
+                # TODO: Add selected_option and the correct option itself. + Question Difficulty?
+            })
 
-                # TODO: [OPTIONAL] Apply GPT feedback/report based on answers provided by user
+            # TODO: [OPTIONAL] Apply GPT feedback/report based on answers provided by user
 
-    return jsonify({"incorrect_answers": incorrect_answers})
+    return jsonify({"evaluation_response": evaluation_response})
+
+
+def serialize_option(option):
+    return {
+        "key": option.key,
+        "value": option.value,
+    }
